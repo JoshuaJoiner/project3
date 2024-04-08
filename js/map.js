6c7436d6a38beee38145b5720ee7ca5d9bf68c83
JoshuaJoiner <156824162+JoshuaJoiner@users.noreply.github.com>
2024-04-08 14:53:34 -0500

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,33 @@
+// Add markers
+var marker1 = new google.maps.Marker({
+    position: { lat: 40.7128, lng: -74.0060 },
+    map: map,
+    title: 'Chicago'
+});
+
+var marker2 = new google.maps.Marker({
+    position: { lat: 34.0522, lng: -118.2437 },
+    map: map,
+    title: 'Los Angeles'
+});
+
+// Custom map styling
+var customStyle = [
+    {
+        featureType: 'poi',
+        elementType: 'labels',
+        stylers: [{ visibility: 'off' }]
+    }
+];
+
+map.setOptions({ styles: customStyle });
+
+// Info windows
+var infoWindow = new google.maps.InfoWindow({
+    content: '<h3>Chicago</h3><p>The most populous city in the United States.</p>'
+});
+
+// Add click event to marker
+marker1.addListener('click', function() {
+    infoWindow.open(map, marker1);
+});
