7216356906b91c78588e13a6d4dab2a2b611988d
JoshuaJoiner <156824162+JoshuaJoiner@users.noreply.github.com>
2024-04-08 15:13:00 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -14,18 +14,6 @@ var marker = new google.maps.Marker({
     title: 'Chicago'
 });
 
-// Info window for Chicago marker
-var infoWindow = new google.maps.InfoWindow({
-    content: '<h3>Chicago</h3><p>The Windy City</p>'
-});
-
-// Open info window when marker is clicked
-marker.addListener('click', function() {
-    infoWindow.open(map, marker);
-});
-
-});
-
 var marker2 = new google.maps.Marker({
     position: { lat: 34.0522, lng: -118.2437 },
     map: map,
