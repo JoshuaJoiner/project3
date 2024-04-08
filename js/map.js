2ec9c23c95c131aa41ac0bd025a16138c17faabc
JoshuaJoiner <156824162+JoshuaJoiner@users.noreply.github.com>
2024-04-08 15:11:48 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,8 +1,29 @@
-// Add markers
-var marker1 = new google.maps.Marker({
+// Map options
+var options = {
+    center: { lat: 41.8781, lng: -87.6298 }, // Chicago coordinates
+    zoom: 12
+};
+
+// Create map
+var map = new google.maps.Map(document.getElementById('map'), options);
+
+// Add marker for Chicago
+var marker = new google.maps.Marker({
     position: { lat: 41.8781, lng: -87.6298 },
     map: map,
     title: 'Chicago'
+});
+
+// Info window for Chicago marker
+var infoWindow = new google.maps.InfoWindow({
+    content: '<h3>Chicago</h3><p>The Windy City</p>'
+});
+
+// Open info window when marker is clicked
+marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+});
+
 });
 
 var marker2 = new google.maps.Marker({
@@ -24,7 +45,7 @@ map.setOptions({ styles: customStyle });
 
 // Info windows
 var infoWindow = new google.maps.InfoWindow({
-    content: '<h3>Chicago</h3><p>The most populous city in the United States.</p>'
+    content: '<h3>Chicago</h3><p>The Windy City. Home of the bulls.</p>'
 });
 
 // Add click event to marker
