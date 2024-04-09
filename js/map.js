9f221ebe8d3ad7b585041fff3c26c33fd495f04f
JoshuaJoiner <156824162+JoshuaJoiner@users.noreply.github.com>
2024-04-09 09:26:03 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,18 +1,36 @@
- // Initialize and add the map
-        function initMap() {
-            // The location to center the map (Chicago)
-            var centerLocation = { lat: 41.8781, lng: -87.6298 }; // Chicago coordinates
+function init(){
+	//alert('it works');
+	var el = document.getElementById('canvas');
+	var myLocation = new google.maps.LatLng(41.83566926343903, -87.62569531405664);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
 
-            // The map, centered at Chicago
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 12, // Adjust the zoom level as desired
-                center: centerLocation
-            });
+	var myMap = new google.maps.Map(el, mapOptions);
 
-            // Optionally, you can add markers or other map features here
-            var marker = new google.maps.Marker({
-                position: centerLocation,
-                map: map,
-                title: 'Chicago'
-            });
-        }
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		animation: google.maps.Animation.BOUNCE,
+		icon: 'images/iit-logo.png'
+	});
+
+	var contentString = '<h1>IIT MTCC</h1><p>MTCC is one of the best hangout and study spots on campus.</p>';
+
+	var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	});
+
+	google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
+
+
+}
+
+google.maps.event.addDomListener(window, 'load', init);
